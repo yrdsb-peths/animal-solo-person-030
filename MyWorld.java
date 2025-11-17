aade8b19e9c25760e8e606c35b5c77ef42d9a193
person-030 <abigailyang783@gmail.com>
2025-11-17 14:04:42 -0500

    apple and elephant

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,13 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant e = new Elephant();
+        addObject(e, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple,300,0);
+        
+        
     }
 }
